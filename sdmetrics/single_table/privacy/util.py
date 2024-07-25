61d18536e3f95183611cfb00515f78fcf6d5be03
Amir Amine <91593233+iamamiramine@users.noreply.github.com>
2024-07-25 10:18:22 +0200

    Update util.py

--- sdmetrics/single_table/privacy/util.py
+++ sdmetrics/single_table/privacy/util.py
@@ -122,6 +122,7 @@ def allow_nan(df):
     df_copy = df.copy()
     for i in df_copy:
         for j in range(len(df_copy[i]) - 1):
+            print(df_copy, flush=True)
             entry = df_copy[i][j]
             if (isinstance(entry, float) and np.isnan(entry)) or entry is None:
                 df_copy[i][j] = 'place_holder_for_nan'
